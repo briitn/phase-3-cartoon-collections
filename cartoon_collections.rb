653e409a022b2d62495e948f1163b6117ad8edc3
princegobah <babynamek@gmail.com>
2022-06-03 23:26:11 -0400

    commit tree eb87f1561b0291d9536965441d33fd4b6083683d

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,33 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arg)
+  arg.each_with_index do |item, index|
+    puts "#{index+1}. #{item}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arg)
+  arg.map do |item|
+    item.capitalize + "!"
+    end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arg)
+  result=false
+arg.each do |item|
+  if item.length>4
+   result= true
+
+  end
+  
+end
+result
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arg)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
+arg.find do |item|
+ 
+  cheese_types.include?item
 end
+end
+find_the_cheese(["crackers", "gouda", "thyme"])
